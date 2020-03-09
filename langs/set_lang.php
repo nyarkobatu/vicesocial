5ba17bab2ba792289406457c975e3ff3ac6dab27
nyarkobatu <54336058+nyarkobatu@users.noreply.github.com>
2020-03-09 12:56:41 +0300

    beta v0.1

---
+++ langs/set_lang.php
@@ -0,0 +1,17 @@
+<?php
+switch ($_SESSION['language']) {
+case 'English':
+	include_once $path."langs/english.php";
+	break;
+case 'العربية':
+	include_once $path."langs/arabic.php";
+	break;
+case 'Turkish':
+	include_once $path."langs/turkish.php";
+	break;
+default:
+	$_SESSION['language'] = "Turkish";
+	include_once $path."langs/turkish.php";
+	break;
+}
+?>
